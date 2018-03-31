8e211a54edceb03f922b28698a17975cf011285f
Yichen <yichen.ece@gmail.com>
2018-03-31 13:29:25 -0400

    use correct IP address

--- service/main.go
+++ service/main.go
@@ -34,8 +34,8 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.188.203.239:9200"
-	FILTER_KEYWORD = "spam"
+	ES_URL = "http://35.184.14.163:9200"
+	//FILTER_KEYWORD = "spam"
 )
 
 
@@ -175,10 +175,10 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		p := item.(Post) // p = (Post) item
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
 		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
-		if !strings.constains(p.Message, FILTER_KEYWORD){
-			ps = append(ps, p)
-		}
-
+		//if !strings.constains(p.Message, FILTER_KEYWORD){
+		//	ps = append(ps, p)
+		//}
+		ps = append(ps, p)
 
 	}
 	js, err := json.Marshal(ps)
